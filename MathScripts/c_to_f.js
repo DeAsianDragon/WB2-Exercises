88b5ea552fa2ba807ef38b0670440a7e69c25974
DeAsianDragon <joshlin.ling@yahoo.com>
2023-10-18 11:39:33 -0700

    temperature

---
+++ MathScripts/c_to_f.js
@@ -0,0 +1,7 @@
+//input
+var celsuis = 25
+//equation
+var fahrenheit = celsuis * (9/5) + 32
+
+//output
+console.log("Celsuis of " + celsuis + " is " + fahrenheit + " in fahrenheit" )
---
+++ MathScripts/f_to_c.js
@@ -0,0 +1,7 @@
+//input
+var fahrenheit = 85
+//equation
+var celsuis = (fahrenheit - 32) * (5/9)
+
+//output
+console.log("Fahrenheit of " + fahrenheit + " is " + celsuis.toFixed(2) + " in celsuis" )
