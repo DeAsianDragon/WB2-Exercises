4b9ba6cf4aa81e79423f5a22b77451fbb7489165
DeAsianDragon <joshlin.ling@yahoo.com>
2023-10-18 10:42:08 -0700

    tip ammount

---
+++ MathScripts/tip_amount.js
@@ -0,0 +1,10 @@
+//input
+var foodCostTotal = 25.00
+
+//equation
+var tipTotal = foodCostTotal * 0.15
+
+//output
+console.log(
+    "The tip on a $"+ foodCostTotal 
++ " food bill is $" + tipTotal)
