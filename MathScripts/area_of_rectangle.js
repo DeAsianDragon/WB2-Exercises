19fb106c2d99eb31d58e8d932e605566fd627d7f
DeAsianDragon <joshlin.ling@yahoo.com>
2023-10-18 10:36:49 -0700

    rectangle done

---
+++ MathScripts/area_of_rectangle.js
@@ -0,0 +1,9 @@
+//input
+var rectangleLength = 5
+var rectangleWidth = 8
+
+//equation
+var rectangleArea = rectangleLength * rectangleWidth
+
+//output
+console.log("The area of the rectangle is " + rectangleArea)
--- MathScripts/net_worth.js
+++ MathScripts/net_worth.js
@@ -1,4 +1,4 @@
-
+var netWorth = 999.99
 
 
 
