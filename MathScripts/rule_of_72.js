204a51490e6e787753c6f2e0c30ae7ff3b0fc845
DeAsianDragon <joshlin.ling@yahoo.com>
2023-10-18 11:25:05 -0700

    rule of 72

---
+++ MathScripts/rule_of_72.js
@@ -0,0 +1,11 @@
+var interestRate = 8
+var inBank = 250
+
+//equation
+var totalWorth = inBank * 2
+var years = 72 / interestRate
+
+//output
+console.log ("At a(n) " + interestRate 
++ "% interest rate, your savings account will be worth " 
++ totalWorth + " in " + years + " years")
